d5065ab869a1345fba1bdc591340d28f67e903fc
amsalinas03 <73053309+amsalinas03@users.noreply.github.com>
2021-06-24 21:17:08 -0500

    Update apiRoutes.js

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -4,4 +4,21 @@ const noteData = require('../db/db.json')
 const express = require('express')
 const router = express.Router()
 
-//
+//import functions from store
+import { getNotes, addNotes} from ('../db/store');
+
+//shows notes from db
+router.get('/notes', (req, res) => {
+    getNotes()
+    .then((notes) => {
+    return res.json(notes);
+    })
+  });
+
+//route to add notes from db
+router.post('/notes', (req, res) => {
+    addNote(req.body)
+      .then((note) => res.json(note))
+  });
+
+module.exports = router;
