a7b8bdb14066e37f8e83e3e84cb4ee5f5e4e313f
amsalinas03 <73053309+amsalinas03@users.noreply.github.com>
2021-06-24 21:51:15 -0500

    added router to package.json

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,5 +1,3 @@
-//import db.json where notes are held
-const noteData = require('../db/db.json')
 //dependencies
 const express = require('express')
 const router = express.Router()
--- Develop/server.js
+++ Develop/server.js
@@ -10,7 +10,9 @@ app.use(express.json());
 
 //Tells server how to respond when user visits certain pages
 require('./routes/htmlRoutes')(app);
-require('./routes/apiRoutes')(app);
+//tells app to use apiRoutes to connect to db
+const apiRoutes = require('./routes/apiRoutes')
+app.use('/api', apiRoutes);
 //start server
 app.listen(PORT, () => {
     console.log(`now listening on http://localhost:${PORT}`)
