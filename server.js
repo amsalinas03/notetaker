6210de68e6f28d4634d24b73d9fdc963f8b8c59c
amsalinas03 <73053309+amsalinas03@users.noreply.github.com>
2021-07-01 21:10:49 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 //define a port for testing
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 //Sets up express to handle data parsing
 app.use(express.urlencoded({ extended: true }));
